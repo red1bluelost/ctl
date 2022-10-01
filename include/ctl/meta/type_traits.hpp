06bbca796b80db895c71b53eea74a096e6a28754
Micah Weston <micahsweston@gmail.com>
2022-09-30 23:48:00 -0400

    Adds documentation and example for enable_same.

--- include/ctl/meta/type_traits.hpp
+++ include/ctl/meta/type_traits.hpp
@@ -4,8 +4,9 @@
 //
 //===----------------------------------------------------------------------===//
 ///
-/// This file contains useful metaprogramming type traits beyond the regular
-/// standard library.
+/// \file
+/// This file contains useful metaprogramming type traits beyond the
+/// regular standard library.
 ///
 //===----------------------------------------------------------------------===//
 #ifndef CTL_META_TYPE_TRAITS_HPP
@@ -15,11 +16,27 @@
 
 namespace ctl {
 
-template<typename Unknown, typename Type, typename Return = void>
-struct enable_same : std::enable_if<std::is_same_v<Unknown, Type>, Return> {};
+//===----------------------------------------------------------------------===//
+// Combinations for \c std::enable_if and type_trait predicates.
+//===----------------------------------------------------------------------===//
 
-template<typename Unknown, typename Type, typename Return = void>
-using enable_same_t = typename enable_same<Unknown, Type, Return>::type;
+/// \brief Combination of \c std::enable_if and \c std::is_same.
+///
+/// Example usage:
+/// \code
+/// template<typename T, ctl::enable_same_t<T, int, bool> = true>
+/// int only_int_add(T l, T r) { return l + r; }
+/// \endcode
+///
+/// \tparam T One of two types to compare if same
+/// \tparam U Second of two types to compare if same
+/// \tparam R Type returned if the compared types were the same
+template<typename T, typename U, typename R = void>
+struct enable_same : std::enable_if<std::is_same_v<T, U>, R> {};
+
+/// Alias template for \c enable_same.
+template<typename T, typename U, typename R = void>
+using enable_same_t = typename enable_same<T, U, R>::type;
 
 } // namespace ctl
 
