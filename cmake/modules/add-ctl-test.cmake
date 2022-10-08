89e8eb3cb7f6c0fac025afdd4d05531cf01d6b77
Micah Weston <micahsweston@gmail.com>
2022-10-08 19:15:42 -0400

    Makes the cmake modules generic to more than just the project.

--- cmake/modules/add-ctl-test.cmake
+++ cmake/modules/add-ctl-test.cmake
@@ -1,9 +1,10 @@
 include_guard()
 
-function (ctl_add_test name)
+function (${PROJECT_NAME}_add_test name)
   cmake_parse_arguments(ARG "" "TEST_FILES" "" ${ARGN})
 
-  add_executable(ctl_${name}_test ${ARG_TEST_FILES})
-  target_link_libraries(ctl_${name}_test PRIVATE ctl_${name} gtest_main)
-  gtest_discover_tests(ctl_${name}_test)
+  add_executable(${PROJECT_NAME}_${name}_test ${ARG_TEST_FILES})
+  target_link_libraries(${PROJECT_NAME}_${name}_test
+                        PRIVATE ${PROJECT_NAME}_${name} gtest_main)
+  gtest_discover_tests(${PROJECT_NAME}_${name}_test)
 endfunction ()
--- cmake/modules/add-ctl.cmake
+++ cmake/modules/add-ctl.cmake
@@ -1,17 +1,16 @@
 include_guard()
 
-function (ctl_add_component name)
+function (${PROJECT_NAME}_add_component name)
   cmake_parse_arguments(ARG "" "INTERFACE_HEADER_FILES" "" ${ARGN})
   set(libname ${PROJECT_NAME}_${name})
   foreach (header ${ARG_INTERFACE_HEADER_FILES})
     add_library(${libname} INTERFACE
-                ${PROJECT_SOURCE_DIR}/include/ctl/${name}/${header})
+                ${PROJECT_SOURCE_DIR}/include/${PROJECT_NAME}/${name}/${header})
   endforeach ()
   add_library(${PROJECT_NAME}::${name} ALIAS ${libname})
   target_include_directories(
-    ${libname}
-    INTERFACE $<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/include>
-              $<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}_${name}>)
+    ${libname} INTERFACE $<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/include>
+                         $<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}>)
   target_compile_features(${libname} INTERFACE cxx_std_20)
 
   install(
@@ -21,8 +20,9 @@ function (ctl_add_component name)
     LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
     RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR})
 
-  set_property(GLOBAL APPEND PROPERTY CTL_COMPONENTS ${name})
+  string(TOUPPER ${PROJECT_NAME} PN_UP)
+  set_property(GLOBAL APPEND PROPERTY ${PN_UP}_COMPONENTS ${name})
 
-  install(DIRECTORY ${PROJECT_SOURCE_DIR}/include/ctl/${name}
-          DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}/ctl)
+  install(DIRECTORY ${PROJECT_SOURCE_DIR}/include/${PROJECT_NAME}/${name}
+          DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}/${PROJECT_NAME})
 endfunction ()
