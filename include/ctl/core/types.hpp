b9ac29a05894465ea318647bd2c10b8f1d39adb3
Micah Weston <micahsweston@gmail.com>
2022-11-26 21:54:38 -0500

    Adds more comments to basic types.

--- include/ctl/core/types.hpp
+++ include/ctl/core/types.hpp
@@ -23,28 +23,38 @@ CTL_BEGIN_NAMESPACE
 
 inline namespace core {
 
-/// \brief Aliases for platform specific size types. These are commonly used
-/// when indexing containers and checking sizes.
+/// \brief Alias for platform specific unsigned size types. These are commonly
+/// used when indexing containers and checking sizes.
 using usize = std::size_t;
+/// \brief Alias for platform specific signed size types. These are commonly
+/// used when indexing containers and subtracting pointers.
 using isize = std::ptrdiff_t;
 
-/// \brief Aliases for fixed size unsigned integral types.
+/// \brief Aliases for fixed size unsigned 8 bit integral.
 using u8  = std::uint8_t;
+/// \brief Aliases for fixed size unsigned 16 bit integral.
 using u16 = std::uint16_t;
+/// \brief Aliases for fixed size unsigned 32 bit integral.
 using u32 = std::uint32_t;
+/// \brief Aliases for fixed size unsigned 64 bit integral.
 using u64 = std::uint64_t;
 // TODO: add optional support for u128
 
-/// \brief Aliases for fixed size signed integral types.
+/// \brief Aliases for fixed size signed 8 bit twos-complement integral.
 using i8  = std::int8_t;
+/// \brief Aliases for fixed size signed 16 bit twos-complement integral.
 using i16 = std::int16_t;
+/// \brief Aliases for fixed size signed 32 bit twos-complement integral.
 using i32 = std::int32_t;
+/// \brief Aliases for fixed size signed 64 bit twos-complement integral.
 using i64 = std::int64_t;
 // TODO: add optional support for i128
 
-/// \brief Aliases for fixed size floating point types.
+/// \brief Aliases for fixed size 32-bit floating point type.
 using f32  = float;
+/// \brief Aliases for fixed size 64-bit floating point type.
 using f64  = double;
+/// \brief Aliases for fixed size 128-bit floating point type.
 using f128 = long double;
 // TODO: disable f128 if long double is not supported
 
