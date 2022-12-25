6d42d1983c8d3fdb185eb13dc4ca0a1439cb2205
Micah Weston <micahsweston@gmail.com>
2022-12-24 20:25:12 -0500

    Improves out_var tests.

--- unittests/object/parameter_test.cpp
+++ unittests/object/parameter_test.cpp
@@ -17,17 +17,46 @@
 
 namespace {
 
-consteval void test_out_var_consteval(int& i) { i = 3; }
-consteval int  testing_out_var_consteval() {
+struct Base {
   int i = 0;
-  test_out_var_consteval(i);
+};
+struct DerivedPublic : public Base {};
+
+consteval void test_out_var_consteval_ref(int& i) { i = 3; }
+consteval int  testing_out_var_consteval_ref() {
+  int i = 0;
+  test_out_var_consteval_ref(ctl::out_var(i));
   return i;
 }
-constexpr void   test_out_var_constexpr(double& i) { i = 42.1; }
-consteval double testing_out_var_constexpr() {
+consteval void test_out_var_consteval_ptr(int* i) { *i = 3; }
+consteval int  testing_out_var_consteval_ptr() {
+  int i = 0;
+  test_out_var_consteval_ptr(ctl::out_var(i));
+  return i;
+}
+constexpr void   test_out_var_constexpr_ref(double& d) { d = 42.1; }
+consteval double testing_out_var_constexpr_ref() {
   double d = 0;
-  test_out_var_constexpr(d);
+  test_out_var_constexpr_ref(ctl::out_var(d));
   return d;
+}
+constexpr void   test_out_var_constexpr_ptr(double* d) { *d = 42.1; }
+consteval double testing_out_var_constexpr_ptr() {
+  double d = 0;
+  test_out_var_constexpr_ptr(ctl::out_var(d));
+  return d;
+}
+consteval void test_out_var_consteval_base_class_ref(Base& b) { b.i = 21; }
+consteval int  testing_out_var_consteval_base_class_ref() {
+  DerivedPublic d{};
+  test_out_var_consteval_base_class_ref(ctl::out_var(d));
+  return d.i;
+}
+consteval void test_out_var_consteval_base_class_ptr(Base* b) { b->i = 21; }
+consteval int  testing_out_var_consteval_base_class_ptr() {
+  DerivedPublic d{};
+  test_out_var_consteval_base_class_ptr(ctl::out_var(d));
+  return d.i;
 }
 TEST(parameter_test, out_var_constexpr) {
   static constexpr int i = 3;
@@ -41,15 +70,20 @@ TEST(parameter_test, out_var_constexpr) {
   static_assert(ip == &i);
   static_assert(*ip == i);
 
-  static_assert(testing_out_var_consteval() == 3);
-  static_assert(testing_out_var_constexpr() == 42.1);
+  static_assert(testing_out_var_consteval_ref() == 3);
+  static_assert(testing_out_var_constexpr_ref() == 42.1);
+  static_assert(testing_out_var_consteval_base_class_ref() == 21);
+
+  static_assert(testing_out_var_consteval_ptr() == 3);
+  static_assert(testing_out_var_constexpr_ptr() == 42.1);
+  static_assert(testing_out_var_consteval_base_class_ptr() == 21);
 }
 
 class out_var_test : public ::testing::Test {
  protected:
   template<typename Tester>
   void run(std::source_location call_loc = std::source_location::current()) {
-    testing::ScopedTrace _t(
+    testing::ScopedTrace tester_func_scope_trace(
         call_loc.file_name(),
         static_cast<int>(call_loc.line()),
         "running tests for ctl::out_var"
@@ -80,11 +114,17 @@ class out_var_test : public ::testing::Test {
       ASSERT_NE(ip, nullptr);
       ASSERT_EQ(*ip, 34);
     }
+    {
+      DerivedPublic d{};
+      ASSERT_EQ(d.i, 0);
+      Tester::test_base_class(ctl::out_var(d));
+      ASSERT_EQ(d.i, 21);
+    }
   }
 
  private:
   std::source_location tester_loc = std::source_location::current();
-  testing::ScopedTrace _ttr{
+  testing::ScopedTrace tester_scope_trace{
       tester_loc.file_name(),
       static_cast<int>(tester_loc.line()),
       "fixture for testing out_var"};
@@ -99,6 +139,7 @@ TEST_F(out_var_test, function_out_by_ref) {
       static int i = 34;
       ip           = &i;
     }
+    static void test_base_class(Base& b) { b.i = 21; }
   };
   run<ref_tester>();
 }
@@ -112,26 +153,55 @@ TEST_F(out_var_test, function_out_by_ptr) {
       static int i = 34;
       *ip          = &i;
     }
+    static void test_base_class(Base* b) { b->i = 21; }
   };
   run<ptr_tester>();
 }
 
 template<typename T>
-struct view_value {
-  view_value(ctl::out_var<T> ov) : value(ov) {}
+struct view_value_ref {
+  template<std::common_reference_with<T> CommonRefType>
+  view_value_ref(ctl::out_var<CommonRefType> ov) : value(ov) {}
   T& value;
 };
-TEST_F(out_var_test, function_out_by_view) {
+TEST_F(out_var_test, function_out_by_view_ref) {
   struct view_tester {
-    static void test_str(view_value<std::string> s) { s.value = "the result"; }
-    static void test_int(view_value<int> i) { i.value = 42; }
-    static void test_vector(view_value<std::vector<double>> v) {
+    static void test_str(view_value_ref<std::string> s) {
+      s.value = "the result";
+    }
+    static void test_int(view_value_ref<int> i) { i.value = 42; }
+    static void test_vector(view_value_ref<std::vector<double>> v) {
       v.value.push_back(3.4);
     }
-    static void test_int_ptr(view_value<int*> ip) {
+    static void test_int_ptr(view_value_ref<int*> ip) {
       static int i = 34;
       ip.value     = &i;
     }
+    static void test_base_class(view_value_ref<Base> b) { b.value.i = 21; }
+  };
+  run<view_tester>();
+}
+
+template<typename T>
+struct view_value_ptr {
+  template<std::common_reference_with<T> CommonRefType>
+  view_value_ptr(ctl::out_var<CommonRefType> ov) : value(ov) {}
+  T* value;
+};
+TEST_F(out_var_test, function_out_by_view_ptr) {
+  struct view_tester {
+    static void test_str(view_value_ptr<std::string> s) {
+      *s.value = "the result";
+    }
+    static void test_int(view_value_ptr<int> i) { *i.value = 42; }
+    static void test_vector(view_value_ptr<std::vector<double>> v) {
+      v.value->push_back(3.4);
+    }
+    static void test_int_ptr(view_value_ptr<int*> ip) {
+      static int i = 34;
+      *ip.value    = &i;
+    }
+    static void test_base_class(view_value_ptr<Base> b) { b.value->i = 21; }
   };
   run<view_tester>();
 }
